da9b591f28e550352df7afef0e4d6b57f73f6629
carloantoniocc <carloantoniocc@gmail.com>
2015-07-29 17:47:29 -0300

    Validación de entradas
    v7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -3,19 +3,20 @@ var models = require('../models/models');
 // GET /quizes/question
 exports.question = function(req, res) {
 	models.Quiz.findAll().then(function(quiz) {
-		res.render('quizes/question', {pregunta : quiz[0].pregunta});
-	})	
+		res.render('quizes/question', {pregunta : quiz[0].pregunta, errors: [] });
+	})
 }
 
 // GET /quizes/answer
 exports.answer = function(req, res){
-	models.Quiz.findAll().then(function(quiz) {	
+	models.Quiz.findAll().then(function(quiz) {
+		
 		if(req.query.respuesta == quiz[0].respuesta){
 			res.render('quizes/answer', {respuesta: 'Correcto'});
 		}else{
 			res.render('quizes/answer', {respuesta: 'Incorrecto'});
 		}
-	})	
+	})
 }
 
 // GET /quizes/new
@@ -23,35 +24,46 @@ exports.new = function(req, res) {
 	var quiz = models.Quiz.build(// crea un objeto quiz
 		{pregunta: "Pregunta", respuesta: "Respuesta"}
 		);
-		
-		res.render('quizes/new', {quiz:quiz});
+
+		res.render('quizes/new', {quiz:quiz, errors: [] });
 }
 
 //POST /quizes/create
 exports.create = function(req, res) {
 	var quiz = models.Quiz.build(req.body.quiz);
-	
-	// guarda en BD los campos pregunta y respuesta de quiz	
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-		res.redirect('/quizes');
-	})	
+
+	quiz
+	.validate()
+	.then(
+		function(err){
+			if (err){
+				res.render('quizes/new', {quiz:quiz, errors: err.errors})
+			}else{
+				// guarda en BD los campos pregunta y respuesta de quiz
+				quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+					res.redirect('/quizes');
+				})
+			}
+		}
+	)
 }
 
 //GET /quizes/:id
 exports.show = function(req, res) {
 	models.Quiz.findById(req.params.quizId).then(function(quiz) {
-		res.render('quizes/show', {quiz:quiz} );
+		res.render('quizes/show', {quiz:quiz, errors: []} );
 	})
 }
 
+// GET /quizes/:id/answer
 exports.answer = function(req, res) {
 	models.Quiz.findById(req.params.quizId).then(function(quiz) {
 		if (req.query.respuesta === quiz.respuesta) {
 			res.render('quizes/answer',
-						{ quiz: quiz, respuesta : 'Correcto'});
+						{ quiz: quiz, respuesta : 'Correcto', errors:[] });
 		} else {
-			res.render('quizes/answer', 
-						{ quiz:quiz, respuesta: 'Incorrecto'});
+			res.render('quizes/answer',
+						{ quiz:quiz, respuesta: 'Incorrecto', errors:[] });
 		}
 		})
 	}
@@ -60,21 +72,6 @@ exports.answer = function(req, res) {
 // GET /quizes
 exports.index = function(req,res) {
 	models.Quiz.findAll().then(function(quizes) {
-		res.render('quizes/index.ejs', {quizes:quizes});
+		res.render('quizes/index.ejs', {quizes:quizes, errors: [] });
 	})
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
