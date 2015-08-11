bb8375f51dd9df1253aec01c3a81b1648bac7572
carloantoniocc <carloantoniocc@gmail.com>
2015-08-11 10:06:07 -0300

    Autenticación y sesión
    v18

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller')
 var commentController = require('../controllers/comment_controller')
+var sessionController = require('../controllers/session_controller')
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -12,11 +13,16 @@ router.get('/', function(req, res) {
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
 
+// Definicion de rutas para session
+router.get('/login',                            sessionController.new) //formulario login
+router.post('/login',                           sessionController.create) // crear session
+router.get('/logout',                           sessionController.destroy) // destruir la session
+
+// Definicion de rutas para quizes
 router.get('/quizes/question', 				          quizController.question)
 router.get('/quizes/answer',   				          quizController.answer)
 router.get('/quizes/new',      				          quizController.new)
 router.post('/quizes/create',  				          quizController.create)
-
 router.get('/quizes',          				          quizController.index)
 router.get('/quizes/:quizId(\\d+)', 		        quizController.show)
 router.get('/quizes/:quizId(\\d+)/answer', 	    quizController.answer)
@@ -24,6 +30,8 @@ router.get('/quizes/:quizId(\\d+)/edit',        quizController.edit)
 router.put('/quizes/:quizId(\\d+)',             quizController.update)
 router.delete('/quizes/:quizId(\\d+)',          quizController.destroy)
 router.get('/autor',                            quizController.autor)
+
+// Definicion de rutas para comments
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new)
 router.post('/quizes/:quizId(\\d+)/comments',    commentController.create)
 
