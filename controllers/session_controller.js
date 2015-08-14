8f860e1d42a985ecfffa3469c39e410c849dafaf
carloantoniocc <carloantoniocc@gmail.com>
2015-08-14 15:34:11 -0300

    Autorización
    v19

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,5 +1,14 @@
 var models = require('../models/models');
 
+exports.loginRequired = function(req,res, next){
+  if(req.session.user) {
+    next();
+  } else{
+    res.redirect('/login');
+  }
+}
+
+
 // session/new
 exports.new = function(req, res) {
   var errors = req.session.errors || {};
