898e3cc47456c67dbdbf8b19cc2dfa1e89910a60
carloantoniocc <carloantoniocc@gmail.com>
2015-08-17 13:02:15 -0300

    Autorizacion
    v21

--- models/comment.js
+++ models/comment.js
@@ -4,7 +4,7 @@ module.exports = function(sequelize, DataTypes) {
         type : DataTypes.STRING,
         validate: { notEmpty: {msg: "--> Falta Comentario"}}
       },
-      pulbicado: {
+      publicado: {
         type : DataTypes.BOOLEAN,
         defaultValue : false
       }
